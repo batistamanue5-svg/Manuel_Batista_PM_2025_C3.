370b660d70be32b7500c8d18ab23e30bf25f4d40
Manuel Batista <batistamanue5@gmail.com>
2025-09-18 22:03:16 -0400

    Manuel_Batista_PM_2025_C3
    git commit -m Manuel_Batista_PM_2025_C3

---
+++ programa 1.7/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* Medidas.
+el programa, al recibir como datos la longitud y el peso de un objeto expresados en pies y libras, calcula los datos de este objeto pero en metros y kilogramos, respectivamente.
+
+PIE, LIB, MET y KIL: variables de tipo ral. */
+
+void main(void)
+{
+    float PIE, LIB, MET, KIL;
+    printf("Ingrese los datos del objeto: ");
+    scanf("%f %f", &PIE, &LIB);
+    MET = PIE * 0.09290;
+    KIL = LIB * 0.45359;
+    printf("\nDatos del objeto \nLongitus: %5.2f  \t Peso: %5.2f", MET, KIL);
+
+}
